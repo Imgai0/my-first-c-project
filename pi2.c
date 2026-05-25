8b1fc07208db2b51aa4e00e1580d9f3a636cfc8c
unknown <3084367680@qq.com>
2026-05-25 18:58:32 +0800

    1e-6下求pi次数

---
+++ pi2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int sign=1,i=0;
+    double pi=0.0,n=1.0,term=1.0;
+    while (fabs(term)>=1e-6)
+    {
+     pi=pi+term;
+     n=n+2;
+     sign=-sign;
+     term=sign/n;
+     i=i+1;
+    }
+    pi=pi*4;
+    printf("pi=%10.8f,i=%d\n",pi,i);
+    return 0;
+}
